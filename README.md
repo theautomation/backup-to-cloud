6f4344f0b95e85eab9633717601c006b4ecbeb21
theautomation <github@theautomation.nl>
2023-05-28 23:39:59 +0200

    Add new files

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 <img src="https://github.com/theautomation/kubernetes-gitops/blob/main/assets/img/k8s.png?raw=true" alt="K8s" style="height: 30px; width:30px;"/>
 Application running in Kubernetes.
+
+
+This script is using gnupg to encrypt and sign data.
+
+## Create GPG key
+```bash
+gpg --full-generate-key
+```
+
+## Export public GPG key
+```bash
+gpg --armor --export gpg@theautomation.nl > backup-to-cloud_publickey.asc
+```
--- src/script.sh
+++ src/script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #
-# This script will tarball each directory in the given backup folder location, encrypt each tar file with GPG using my own public key and upload those encrypted files to my Dropbox in the cloud.
+# A script to tarball each directory in the given backup directory location, encrypt each tar file with GPG using my own public key and upload those encrypted files to my Dropbox in the cloud.
 #
 # script writtin by Coen Stam.
 # github@theautomation.nl
 #
 
 # should be from ENV
-backup_directory="/c/Users/coen.stam/Documents/github/scripts/test/dir/backup"
+backup_directory="/home/coen/storage-server/coen/backups-test"
 gpg_recipient="gpg@theautomation.nl"
 debug=false
+dropbox_remote_location=/backup-to-cloud
+DROPBOX_ACCESS_TOKEN=""
 
-# Dropbox script location
-dropbox_uploader="/c/Users/coen.stam/Documents/github/scripts/test/dropbox_uploader.sh"
-
+# Dropbox script
+dropbox_uploader="/home/coen/github/backup-to-cloud/src/dropbox_uploader.sh"
 
 log_file="./backup-to-cloud.log"
 
@@ -34,24 +35,35 @@ mkdir -p "$backup_directory"
 
 # Loop through each subdirectory in the parent directory
 for directory in "$backup_directory"/*; do
-    if [ -d "$directory" ]; then
+
+    if [[ $directory == *.tar.gz ]]; then
+        continue  # Skip existing tar.gz files
+    fi
+
+    if [ -d "$directory" ] && [ -n "$(find "$directory" -mindepth 1 -type f)" ]; then
+
         # Get the directory name without the full path
         dir_name=$(basename "$directory")
 
         # Create the output file name with the directory name and current date
         current_date=$(date +"%Y-%m-%d")
-        output_filename="${dir_name}_${current_date}.tar.gz"
-        output_path="${backup_directory}/${output_filename}"
+        tar_filename="${dir_name}_${current_date}.tar.gz"
+        tar_output_path="${backup_directory}/${tar_filename}"
 
         # Create the tar file for the directory
-        tar -czf "$output_path" -C "$directory" .
+        tar -czf "$tar_output_path" -C "$directory" .
 
-        log "Created tar file: $output_filename"
+        log "Created tar file: $tar_filename"
+
+        # Remove the contents of the directory
+        rm -r "${directory:?}/"*
+    else
+        log "Skip tarball for \"$directory\", because there where no files found"
     fi
 done
 
 # Loop through each tar.gz file in the output directory
-for file in "$backup_directory"/*.tar.gz; do
+for file in "$backup_directory"/*"${current_date}.tar.gz"; do
     if [ -f "$file" ]; then
         # Encrypt the tar.gz file using gpg
         gpg --batch --yes --recipient "$gpg_recipient" --output "$file.gpg" --encrypt "$file"
@@ -68,13 +80,17 @@ done
 # Loop through each .gpg file in the directory
 for file in "$backup_directory"/*.gpg; do
     if [ -f "$file" ]; then
-        # Upload the .gpg file to Dropbox using https://github.com/andreafabrizi/Dropbox-Uploader
-        "$dropbox_uploader" upload "$file" "/Destination/Folder"
+        # Upload the file to Dropbox
+        curl -X POST https://content.dropboxapi.com/2/files/upload \
+            --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
+            --header "Dropbox-API-Arg: {\"path\":\"$dropbox_remote_location/$(basename "$file")\",\"mode\":\"add\",\"autorename\":true,\"mute\":false}" \
+            --header "Content-Type: application/octet-stream" \
+            --data-binary @"$file"
 
         if [ $? -eq 0 ]; then
             log "Upload to Dropbox successful: $file"
-            # Remove gpg file when upload succeed
-            rm $file
+            # Remove the gpg file after successful upload
+            rm "$file"
         else
             log "Upload to Dropbox failed for file: $file"
             exit 1
