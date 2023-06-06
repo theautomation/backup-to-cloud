c35032b06c62fa62253cdf93f6eb4934fd46fd65
theautomation <github@theautomation.nl>
2023-06-06 21:38:45 +0200

    Make functions

--- src/script.py
+++ src/script.py
@@ -19,11 +19,11 @@ LOG_FILE_LOCATION = os.getenv('LOG_FILE_LOCATION', './backup-to-cloud.log')
 BACKUP_DIRECTORY = os.getenv(
     'BACKUP_DIRECTORY', '/home/coen/storage-server/coen/backups-test')
 GPG_RECIPIENT = os.getenv('GPG_RECIPIENT', 'gpg@theautomation.nl')
-DROPBOX_REMOTE_LOCATION = os.getenv('DROPBOX_REMOTE_LOCATION', '')
+DROPBOX_REMOTE_LOCATION = os.getenv('DROPBOX_REMOTE_LOCATION')
 DROPBOX_CLIENT_ID = os.getenv('DROPBOX_CLIENT_ID', '')
 DROPBOX_CLIENT_SECRET = os.getenv('DROPBOX_CLIENT_SECRET', '')
 DROPBOX_REFRESH_TOKEN = os.getenv('DROPBOX_REFRESH_TOKEN', '')
-TEST_VAR = os.getenv('TEST_VAR', '')
+
 # Get the log level from the environment variable
 log_level_str = os.getenv('LOG_LEVEL', 'INFO')
 
@@ -42,14 +42,36 @@ console_formatter = logging.Formatter(
 console_handler.setFormatter(console_formatter)
 logging.getLogger().addHandler(console_handler)
 
-# Log variable names and values
-logging.debug("The following environment variables have been configured:\n"
-              "LOG_FILE_LOCATION: %s\n"
-              "BACKUP_DIRECTORY: %s\n"
-              "GPG_RECIPIENT: %s\n"
-              "DROPBOX_REMOTE_LOCATION: %s\n"
-              "DROPBOX_REFRESH_TOKEN: %s",
-              LOG_FILE_LOCATION, BACKUP_DIRECTORY, GPG_RECIPIENT, DROPBOX_REMOTE_LOCATION, DROPBOX_REFRESH_TOKEN)
+
+def log_environment_variables(**variables):
+    """
+    Logs the names and values of the specified variables using the logging module.
+    """
+    log_message = "The following environment variables have been configured:\n"
+    for name, value in variables.items():
+        log_message += f"{name}: {value}\n"
+    logging.debug(log_message)
+
+
+def check_directory_exists(directory):
+    """
+    Checks if directory exists and SystemExit if the backup directory does not exist.
+    """
+    if not os.path.exists(directory):
+        logging.error("Backup directory '%s' does not exist", directory)
+        sys.exit(1)
+    logging.debug("Backup directory exists: %s", directory)
+
+
+def check_required_env_variable(*required_variables):
+    """
+    Check variables and if they have no value, exit.
+    """
+    for var in required_variables:
+        if not var:
+            logging.error(
+                "Please set a value in the required environment variables.")
+            sys.exit(1)
 
 
 def dropbox_upload(file_path, dropbox_path, access_token):
@@ -69,7 +91,6 @@ def dropbox_refresh_access_token(refresh_token, client_id, client_secret, redire
         client_secret=client_secret,
         redirect_uri=redirect_uri,
     )
-
     try:
         refreshed_token = auth_flow.refresh_access_token(refresh_token)
         access_token = refreshed_token.access_token
@@ -79,31 +100,42 @@ def dropbox_refresh_access_token(refresh_token, client_id, client_secret, redire
         print("Error refreshing access token:", e)
         return None
 
-# Check if the directory exists
-if not os.path.exists(BACKUP_DIRECTORY):
-    # Create the directory
-    os.makedirs(BACKUP_DIRECTORY)
-    logging.error("Backup directory: %s does not exist", BACKUP_DIRECTORY)
-    sys.exit(1)
-else:
-    logging.debug("Backup directory exists: %s", BACKUP_DIRECTORY)
 
-def get_var_name(variable):
-    globals_dict = globals()
-    return [
-        var_name for var_name in globals_dict
-        if globals_dict[var_name] is variable
-    ]
-
-def check_empty_env_vars(*args):
-    for variable in args:
-        if variable == '':
-            variable_name = get_var_name(variable)[0] if variable is not None else "Unknown Variable"
-            logging.error("Please set a value in the \"" + variable_name + "\" " + "environment variable.")
-            sys.exit(1)
+def non_empty_directory_path(directory):
+    """
+    Return a (sub)directory inside the input directory that is not empty.
+    """
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path) and os.listdir(item_path):
+            logging.info("Found non-empty directory: %s", item_path)
+            return item_path
 
 
-check_empty_env_vars(DROPBOX_CLIENT_ID)
+def directory_name(directory_path):
+    """
+    Return the directory name of the input directory path.
+    """
+    directory_name = os.path.basename(directory_path)
+    return directory_name
+
+
+log_environment_variables(
+    LOG_FILE_LOCATION=LOG_FILE_LOCATION,
+    BACKUP_DIRECTORY=BACKUP_DIRECTORY,
+    GPG_RECIPIENT=GPG_RECIPIENT,
+    DROPBOX_REMOTE_LOCATION=DROPBOX_REMOTE_LOCATION,
+    DROPBOX_CLIENT_ID=DROPBOX_CLIENT_ID,
+    DROPBOX_CLIENT_SECRET=DROPBOX_CLIENT_SECRET,
+    DROPBOX_REFRESH_TOKEN=DROPBOX_REFRESH_TOKEN
+)
+
+check_required_env_variable(BACKUP_DIRECTORY, GPG_RECIPIENT, DROPBOX_REMOTE_LOCATION,
+                            DROPBOX_CLIENT_ID, DROPBOX_CLIENT_SECRET, DROPBOX_REFRESH_TOKEN)
+
+
+check_directory_exists(BACKUP_DIRECTORY)
+
 
 # Loop through all files and directories within the parent directory
 for item in os.listdir(BACKUP_DIRECTORY):
@@ -121,32 +153,32 @@ for item in os.listdir(BACKUP_DIRECTORY):
             # Get output filename
             output_filename = f"{current_date}_{directory_name}"
 
-            # Create the tar archive with the same name as the directory
-            output_file = os.path.join(
-                BACKUP_DIRECTORY, f"{output_filename}.tar.gz")
-            with tarfile.open(output_file, "w:gz") as tar:
-                tar.add(item_path, arcname=os.path.basename(item_path))
-                tar.close()
+#             # Create the tar archive with the same name as the directory
+#             output_file = os.path.join(
+#                 BACKUP_DIRECTORY, f"{output_filename}.tar.gz")
+#             with tarfile.open(output_file, "w:gz") as tar:
+#                 tar.add(item_path, arcname=os.path.basename(item_path))
+#                 tar.close()
 
-            # Output filename for encrypted file
-            encrypted_filename = f"{output_file}.gpg"
-            # Encrypt the file
-            gpg = gnupg.GPG()
-            with open(output_file, 'rb') as f:
-                status = gpg.encrypt_file(
-                    f, recipients=[GPG_RECIPIENT],
-                    always_trust=True,
-                    output=encrypted_filename)
-            if status.ok:
-                logging.info("File \"" + output_filename +
-                             ".tar.gz\" encrypted successfully.")
-            else:
-                logging.error("Error encrypting the file:", status.status)
+#             # Output filename for encrypted file
+#             encrypted_filename = f"{output_file}.gpg"
+#             # Encrypt the file
+#             gpg = gnupg.GPG()
+#             with open(output_file, 'rb') as f:
+#                 status = gpg.encrypt_file(
+#                     f, recipients=[GPG_RECIPIENT],
+#                     always_trust=True,
+#                     output=encrypted_filename)
+#             if status.ok:
+#                 logging.info("File \"" + output_filename +
+#                              ".tar.gz\" encrypted successfully.")
+#             else:
+#                 logging.error("Error encrypting the file:", status.status)
 
-            # Upload encrypting file to Dropbox
-            dropbox_upload(encrypted_filename, DROPBOX_REMOTE_LOCATION +
-                           "/"+encrypted_filename, DROPBOX_REFRESH_TOKEN)
+#             # # Upload encrypting file to Dropbox
+#             # dropbox_upload(encrypted_filename, DROPBOX_REMOTE_LOCATION +
+#             #                "/"+encrypted_filename, DROPBOX_REFRESH_TOKEN)
 
-        else:
-            # Directory is empty, skip processing
-            logging.info("Empty directory, skipping: %s", item_path)
+#         else:
+#             # Directory is empty, skip processing
+#             logging.info("Empty directory, skipping: %s", item_path)
